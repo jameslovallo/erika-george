387519fd17216ee3e7faa9ed9fd0b88d49075da5
James Lovallo <patrickonparker@gmail.com>
2023-04-27 23:59:29 -0400

    .

--- @/main.js
+++ @/main.js
@@ -3,16 +3,12 @@ import 'https://unpkg.com/ardi/@/components/app-root.js'
 import 'https://unpkg.com/ardi/demos/dialog/dialog.js'
 
 // load all components
-const components = [
-  'app-footer',
-  'app-layout',
-  'app-link',
-  'app-nav',
-  'app-root',
-]
+const components = ['app-footer', 'app-layout', 'app-nav', 'mark-down']
 components.forEach((c) => import(`/@/components/${c}.js`))
 
 // fade in gracefully when components are loaded
-const isDefined = components.map((c) => customElements.whenDefined(c))
+const isDefined = ['app-link', 'app-root', ...components].map((c) =>
+  customElements.whenDefined(c)
+)
 await Promise.allSettled(isDefined)
 document.body.style.opacity = 1
